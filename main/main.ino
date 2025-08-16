d9048b5ecf8576b718b7bad4719a81e3f62330b9
0xA79D2E <11010101@11000110.xyz>
2025-08-15 22:41:22 -0700

    pwm and inverse kinematics logic
    inverse kin for 2d planar joint to ensure accuracy of method

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,37 @@
 #include <cmath>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+//determined experimentally
+#define MG996R_MIN 140  // pulse length count for 0°
+#define MG996R_MID 345  // pulse length count for 90°
+#define MG996R_MAX 560  // pulse length count for 180°
+
+#define MG90S_MIN 100
+#define MG90S_MID 315
+#define MG90S_MAX 520
+
+//PWM channels on PCA9685
+#define j1 0
+#define j2 1
 
 float a1, a2;
 
-void setup() {
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
+void setup() {
+  pwm.begin();
+  pwm.setPWMFreq(50);  // 50 Hz standard for hobby servos
 }
 
-// geometric inverse kinematics of 2d planar joint
+int angleToPulse(int angle, char type) {
+  if (type == 'r') return map(angle, 0, 180, MG996R_MIN, MG996R_MAX);
+  return map(angle, 0, 180, MG90S_MIN, MG90S, MAX);
+}
+
 void setPos(float x, float y) {
 
+  // geometrically derived inverse kinematics of 2d planar joint
   float r, alpha, beta, gamma, q1, q2;
   // a1,a2 = link lengths
   //q1, q2 = joint angles
@@ -25,6 +48,8 @@ void setPos(float x, float y) {
 
   q1 = gamma - beta;
 
+  pwm.setPWM(j1, 0, angleToPulse(q1, 's'));
+  pwm.setPWM(j2, 0, angleToPulse(q2, 's'));
 }
 
 void loop() {
