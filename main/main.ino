d10842332173e968b16fdb4e42bd8072b048a5a4
0xA79D2E <11010101@11000110.xyz>
2025-08-15 22:33:11 -0700

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,30 +1,31 @@
-#include<cmath>
+#include <cmath>
+
+float a1, a2;
 
 void setup() {
 
-// inverse kinematics of 2d planar joint
+}
 
-float x, y, r, alpha, beta, gamma, a1, a2, q1, q2;
-// a1,a2 = link lengths
-//q1, q2 = joint angles
-// x, y = cartesian coordinates of end effector
-r = Math.sqrt(pow(x, 2) + pow(y, 2));
+// geometric inverse kinematics of 2d planar joint
+void setPos(float x, float y) {
 
-alpha = acos( (pow(a1,2) + pow(a2,2) - pow(r,2)) / (2*a1*a2) );
+  float r, alpha, beta, gamma, q1, q2;
+  // a1,a2 = link lengths
+  //q1, q2 = joint angles
+  // x, y = cartesian coordinates of end effector
+  r = Math.sqrt(pow(x, 2) + pow(y, 2));
 
-q2 = pi - alpha;
+  alpha = acos((pow(a1, 2) + pow(a2, 2) - pow(r, 2)) / (2 * a1 * a2));
 
-gamma = atan(y/x);
+  q2 = pi - alpha;
 
-beta = atan( (a2*sin(q2) ) / (a1 + a2*cos(q2)) );
+  gamma = atan(y / x);
 
-q1 = gamma - beta; //case where q2 > 0
+  beta = atan((a2 * sin(q2)) / (a1 + a2 * cos(q2)));
 
-// q1 = gamma + beta; //case where q2 < 0
-
+  q1 = gamma - beta;
 
 }
 
 void loop() {
-
 }
